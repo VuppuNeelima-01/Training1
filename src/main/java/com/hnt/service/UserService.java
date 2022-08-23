050ab8e7fdb1fd2dbff09b55740355b82338ce39
unknown <cogjava3167@iiht.tech>
2022-08-23 13:05:56 +0530

    training

---
+++ src/main/java/com/hnt/service/UserService.java
@@ -0,0 +1,20 @@
+package com.hnt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hnt.UserRepository;
+import com.hnt.entity.User;
+
+@Service
+public class UserService {
+@Autowired
+UserRepository userRepo;
+	public void save(User user) {
+		userRepo.save(user);
+	
+		
+	}
+	
+
+}
